3ae74f81c074ca7814250f26510d11b6be6c678f
Kirill <kirill.rylskij96@gmail.com>
2026-04-11 15:28:24 +0300

    обновление домашки

--- catalog/views.py
+++ catalog/views.py
@@ -1,17 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, "home.html")
 
 
 def contacts(request):
-    if request.method == 'POST':
-        name = request.POST.get('name')
-        phone = request.POST.get('phone')
-        message = request.POST.get('message')
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
         print(f"Сообщение от {name} ({phone}): {message}")
-        return render(request, 'contacts.html', {
-            'message': 'Спасибо за обращение! Мы свяжемся с вами.'
-        })
-    return render(request, 'contacts.html')
+        return render(request, "contacts.html", {"message": "Спасибо за обращение! Мы свяжемся с вами."})
+    return render(request, "contacts.html")

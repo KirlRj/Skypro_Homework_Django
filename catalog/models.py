e62f7cb992609a165026747058a947dbda9c1fdf
Kirill <kirill.rylskij96@gmail.com>
2026-04-12 21:56:48 +0300

    добавление фикстур в админку, корректировка наименования полей, миграция данных в sql

--- catalog/models.py
+++ catalog/models.py
@@ -3,17 +3,17 @@ from django.db import models
 # Create your models here.
 
 class Product(models.Model):
-    product_name = models.CharField(max_length=100, verbose_name='Наименование продукта')
+    name = models.CharField(max_length=100, verbose_name='Наименование продукта')
     description = models.TextField(verbose_name='Описание продукта')
-    product_image = models.ImageField(verbose_name='Изображение продукта')
+    image = models.ImageField(verbose_name='Изображение продукта')
     category = models.CharField(max_length=100, verbose_name='Наименование категории продукта')
     price = models.FloatField(verbose_name='Цена продукта')
     created_at = models.DateField(verbose_name='Дата создания')
     updated_at = models.DateField(verbose_name='Дата последнего изменения')
 
     def __str__(self):
-        return f'{self.product_name}, {self.category}, {self.price}, {self.created_at}, {self.updated_at}'
+        return f'{self.name}, {self.category}, {self.price}, {self.created_at}, {self.updated_at}'
 
 class Category(models.Model):
-    category = models.CharField(max_length=100, verbose_name='Наименование категории')
+    name = models.CharField(max_length=100, verbose_name='Наименование категории')
     description = models.TextField(verbose_name='Описание категории')

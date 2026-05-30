b3c53804fc125e447d51e391528baf6db9f728b3
Kirill <kirill.rylskij96@gmail.com>
2026-05-30 15:37:30 +0300

    выполнение 29 задания

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
 from catalog import views
-
+from django.views.decorators.cache import cache_page
 app_name = "catalog"
 
 urlpatterns = [
     path("", views.HomeListView.as_view(), name="home"),
     path("contacts/", views.ContactsView.as_view(), name="contacts"),
-    path("product/<int:pk>/", views.ProductDetailView.as_view(), name="product_detail"),
+    path("product/<int:pk>/", cache_page(60)(views.ProductDetailView.as_view()), name="product_detail"),
     path("product/create/", views.ProductCreateView.as_view(), name="product_create"),
     path("product/<int:pk>/update/", views.ProductUpdateView.as_view(), name="product_update"),
     path("product/<int:pk>/delete/", views.ProductDeleteView.as_view(), name="product_delete"),
     path('product/<int:pk>/unpublish/', views.product_unpublish, name='product_unpublish'),
+    path("category/<int:category_id>/", views.ProductsByCategory.as_view(), name="product_by_category"),
 ]
